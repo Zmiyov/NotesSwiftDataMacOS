c2b8bbb650f190ca2fa0a9bc6e1b568cb2d13805
Volodymyr <zmiyov88@gmail.com>
2024-07-01 23:47:18 +0300

    Done

---
+++ NotesSwiftDataMacOS/View/NotesView.swift
@@ -0,0 +1,18 @@
+//
+//  NotesView.swift
+//  NotesSwiftDataMacOS
+//
+//  Created by Volodymyr Pysarenko on 01.07.2024.
+//
+
+import SwiftUI
+
+struct NotesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotesView()
+}
